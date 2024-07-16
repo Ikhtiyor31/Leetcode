ec20c39279f58a3165deda0e0743974adde528aa
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-07-16 10:28:02 +0900

    Time: 2 ms (54.04%) | Memory: 41.5 MB (27.25%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+         
+         Stack<Character> stack = new Stack<>();
+
+         for (Character ch: s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (!stack.isEmpty()) {
+                    if (ch == ')' && stack.peek() == '(') {
+                        stack.pop();
+                    } else if (ch == '}' && stack.peek() == '{') {
+                        stack.pop();
+                    } else if (ch == ']' && stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                   
+                } else {
+                         
+                    return false;
+                }
+            }
+         }
+
+         return stack.isEmpty();
+    }
+}
