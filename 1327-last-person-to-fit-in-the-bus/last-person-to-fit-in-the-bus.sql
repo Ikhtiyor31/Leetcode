4d37785db4bbb1dca982092a0db6f736db12d900
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-07-19 09:44:55 +0900

    Time: 698 ms (89.87%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,24 +1,20 @@
 # Write your MySQL query statement below
 
+ WITH CTE AS (SELECT 
+    person_name,
+    SUM(weight) OVER (ORDER BY turn ASC) as rollingSum,
+    turn
+FROM 
+    Queue)
 
-
-
-WITH CTE AS (
-    SELECT 
-        person_name,
-        rollingSum,
-        ROW_NUMBER() OVER (ORDER BY turn DESC) AS rn
-    FROM (
-        SELECT 
-            person_name,
-            SUM(weight) OVER (ORDER BY turn ASC) as rollingSum,
-            turn
-        FROM 
-            Queue
-    ) subquery
-    WHERE rollingSum <= 1000
-)
-
-SELECT person_name FROM CTE WHERE rn = 1
+SELECT
+    person_name
+FROM 
+    CTE
+WHERE
+    rollingSum <= 1000
+ORDER BY
+    rollingSum DESC
+LIMIT 1
 
 
