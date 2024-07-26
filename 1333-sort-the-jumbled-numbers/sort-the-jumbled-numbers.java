4be411d77984b10a8ae4557704d8f3a3e401d917
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-07-26 16:03:30 +0900

    Time: 229 ms (34.70%) | Memory: 55.7 MB (36.76%) - LeetSync

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -0,0 +1,71 @@
+class Triple {
+    Integer first;
+    Integer second;
+    Integer third;
+    
+    public Triple(Integer first, Integer second, Integer third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public Integer getFirst() {
+        return first;
+    }
+
+    public Integer getSecond() {
+        return second;
+    }
+
+    public Integer getThird() {
+        return third;
+    }
+
+    @Override
+    public String toString() {
+        return "Triple{" +
+            "first=" + first +
+            ", second=" + second +
+            ", third=" + third +
+            '}';
+    }
+}
+class Solution {
+    public int[] sortJumbled(int[] mapping, int[] nums) {
+        List<Triple> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int copyNum = num;
+            int result = 0;
+            int placeValue = 1;
+            StringBuilder sb = new StringBuilder();
+            while(copyNum > 0) {
+                int digit = copyNum % 10;
+                sb.append(mapping[digit]);
+                copyNum /= 10;
+            }
+
+          
+          
+            if (num == 0) {
+                result = mapping[result];
+            } else {
+                result = Integer.parseInt(sb.reverse().toString());
+            }
+
+            
+            list.add(new Triple(result, num, i));
+        }
+
+        list.sort(Comparator
+        .comparing(Triple::getFirst)
+        .thenComparing(Triple::getThird));
+       
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = list.get(i).getSecond();
+        }        
+      
+        return nums;
+    }
+}
