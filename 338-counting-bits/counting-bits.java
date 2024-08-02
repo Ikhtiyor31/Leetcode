0d7adad61b949d20c8725ece4e818ccfd082d87e
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-08-02 12:07:10 +0900

    Time: 1 ms (99.53%) | Memory: 46.2 MB (88.35%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -3,7 +3,7 @@ class Solution {
         
         int[] result = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            result[i] = result[i / 2] + (i & 1);
+            result[i] = result[i >> 1] + (i & 1);
         }
 
         return result;
