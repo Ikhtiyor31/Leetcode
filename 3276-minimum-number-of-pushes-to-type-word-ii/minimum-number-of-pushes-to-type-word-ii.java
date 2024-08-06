d1a6650a9073d24db2be0b002b43ea5f2c4bd977
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-08-06 22:42:00 +0900

    Time: 8 ms (91.92%) | Memory: 45.4 MB (79.42%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -14,8 +14,7 @@ class Solution {
         int pushes = 1;
         int answer = 0;
         for (int i = 0; i < 26; i++) {
-            count++;
-            if (count != 1 && (count - 1) % 8 == 0) {
+            if (i != 0 && i % 8 == 0) {
                 pushes++;
             }
             answer += (sortedFrequency[i] * pushes);
