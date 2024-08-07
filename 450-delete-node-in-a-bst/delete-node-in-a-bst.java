8672f02340491ee96198bed1c26010f8cb42b86b
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-08-07 23:09:37 +0900

    Time: 1 ms (100.00%) | Memory: 46 MB (40.23%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        } else if (root.val == key) {
+            // if no child 
+            if (root.left == null) {
+                return root.right;
+            }
+
+            // if only left child 
+            if (root.right == null) {
+                return root.left;
+            }
+
+            // if both children there 
+            TreeNode minNode = getMinNode(root.right);
+            System.out.println(minNode.val);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+
+        return root;
+    }
+
+    private TreeNode getMinNode(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null && curr.left != null) {
+            curr = curr.left;
+        }
+        return  curr;
+    }
+}
