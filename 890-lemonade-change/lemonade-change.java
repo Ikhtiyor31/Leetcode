6245ba797c2d0392a85412c587c8baa53ef786b1
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-08-15 18:33:10 +0900

    Time: 1 ms (100.00%) | Memory: 56.1 MB (18.72%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,26 +1,29 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
         int n = bills.length;
-        Map<Integer, Integer> changes = new HashMap<>();
-        changes.put(5, 0);
-        changes.put(10, 0);
+        int fiveDollars = 0;
+        int tenDollars = 0;
         for (int bill: bills) {
-            int change = bill - 5;
-            int i = 0;
-            while (changes.get(10) > 0 && change >= 10) {
-                changes.put(10, changes.get(10) - 1);
-                change -= 10;
+            if (bill == 5) {
+                fiveDollars++;
+            } else if (bill == 10) {
+                if (fiveDollars > 0) {
+                    fiveDollars--;
+                    tenDollars++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (tenDollars > 0 && fiveDollars > 0) {
+                    tenDollars--;
+                    fiveDollars--;
+                } else if (fiveDollars >= 3) {
+                    fiveDollars -= 3;
+                } else {
+                    return false;
+                }
             }
-
-            while (changes.get(5) > 0 && change >= 5) {
-                changes.put(5, changes.get(5) - 1);
-                change -= 5;
-            }
-            changes.put(bill, changes.getOrDefault(bill, 0) + 1);
-            if (change != 0) 
-                return false;
         }
-
         return true;
     }
 }
