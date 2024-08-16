6ffeac9c558a2c2fa1f7b698a3d90184411a162f
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-08-16 12:25:32 +0900

    Time: 7 ms (82.70%) | Memory: 64.2 MB (77.64%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
@@ -6,32 +6,15 @@ class Solution {
         int firstMax = arrays.get(0).get(arrays.get(0).size()-1);
         int firstMinIndex = 0;
         int firstMaxIndex = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             int min1 = arrays.get(i).get(0);
             int max1 = arrays.get(i).get(arrays.get(i).size() - 1);
-            if (firstMin > min1) {
-                firstMin = min1;
-                firstMinIndex = i;
-            }
-
-            if (firstMax < max1) {
-                firstMax = max1;
-                firstMaxIndex = i;
-            }
+            answer = Math.max(answer, Math.abs(firstMax - min1));
+            answer = Math.max(answer, Math.abs(firstMin - max1));
+            firstMin = Math.min(firstMin, min1);
+            firstMax = Math.max(firstMax, max1);
         }
-        
-        for (int i = 0; i < n; i++) {
-            int min1 = arrays.get(i).get(0);
-            int max1 = arrays.get(i).get(arrays.get(i).size() - 1);
-            if (firstMaxIndex != i) {
-                answer = Math.max(answer, Math.abs(firstMax - min1));
-            }
 
-            if (firstMinIndex != i) {
-                answer = Math.max(answer, Math.abs(firstMin - max1));
-            }
-        }
-        
         return answer;
     }
 }
