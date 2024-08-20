5f808328df6071dbe96ac101d307c1cc68cf0f56
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-08-20 17:56:44 +0900

    Time: 2 ms (71.33%) | Memory: 42 MB (8.38%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        
+        Arrays.sort(citations);
+
+        int count = 0;
+        for (int i = citations.length - 1; i >= 0; i--) {
+            if (citations[i] <= count) {
+                break;
+            }
+            count++;
+        }
+
+
+        return count;
+    }
+}
