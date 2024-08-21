6265c9accefda8b86ffb0f2377556d6d18a801db
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-08-21 10:16:28 +0900

    Time: 2 ms (71.36%) | Memory: 41.4 MB (65.93%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -4,14 +4,17 @@ class Solution {
         Arrays.sort(citations);
 
         int count = 0;
-        for (int i = citations.length - 1; i >= 0; i--) {
-            if (citations[i] <= count) {
-                break;
+        int n = citations.length;
+        int l = 0;
+        int h = n-1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (citations[mid] >= n-mid) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
             }
-            count++;
         }
-
-
-        return count;
+        return n-l;
     }
 }
