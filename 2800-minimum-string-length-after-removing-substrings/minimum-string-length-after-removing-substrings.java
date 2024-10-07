ef443530c2537be3b114b99d3304ce271adf062a
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-10-07 10:22:51 +0900

    Time: 9 ms (17.26%) | Memory: 44.7 MB (18.31%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,18 +1,10 @@
 class Solution {
     public int minLength(String s) {
-        boolean processed = false;
-        while (!processed) {
-            int n = s.length();
+        while (s.contains("AB") || s.contains("CD")) {
             if (s.indexOf("AB") != -1) {
-                int indx = s.indexOf("AB");
-                s = s.substring(0, indx) + s.substring((int)Math.min(indx+2, n), n);
-                processed = false;
+                s = s.replace("AB", "");
             } else if (s.indexOf("CD") != -1) {
-                int indx = s.indexOf("CD");
-                s = s.substring(0, indx) + s.substring((int)Math.min(indx+2, n), n);
-                processed = false;
-            } else {
-                processed = true;
+                s = s.replace("CD", "");
             }
         }
 
