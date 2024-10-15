6e408c89abbfdc9d5293971f09460de6004032c2
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-10-15 15:26:28 +0900

    Time: 7 ms (100.00%) | Memory: 45.4 MB (54.12%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -1,39 +1,17 @@
 class Solution {
     public long minimumSteps(String s) {
-        int black = 0;
-        int white = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '1')
-                black += 1;
-            else
-                white += 1;
-        }
+        
         int n = s.length();
-        long whiteCount = 0;
-
-        // go with white
-        int indx = 0;
-        while (indx < n && s.charAt(indx) != '1') {
-                indx++;
-        }
+        long count = 0;
+        int whiteCount = 0;
         for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == '0' && indx < i) {
-                whiteCount += (i - indx);
-                indx++;
+            if (s.charAt(i) == '0') {
+                count += i - whiteCount;
+                whiteCount++;
             }
         }
-        long blackCount = 0;
-        indx = n;
-        while (indx > 0 && s.charAt(indx - 1) != '0') {
-                indx--;
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            if (s.charAt(i) == '1' && indx > i) {
-                blackCount += (indx - i);
-            }
-        }
-      
-        return Math.min(whiteCount, blackCount);
+
+        return count;
     }
 }
 
