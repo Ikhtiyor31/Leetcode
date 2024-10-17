c759ccb86575def443618305f5016fb08a52e56a
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-10-17 12:33:30 +0900

    Time: 1 ms (30.39%) | Memory: 39.6 MB (99.76%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -1,42 +1,29 @@
 class Solution {
     public int maximumSwap(int num) {
         
-        
-        StringBuilder nums = new StringBuilder(Integer.toString(num));
-        int n = nums.length();
-        if (n == 1) {
-            return num;
+        String numStr = Integer.toString(num);
+        char[] digits = numStr.toCharArray();
+        int n = digits.length;
+        int[] last = new int[10];
+        for (int i = 0; i < n; i++) {
+            last[digits[i] - '0'] = i;
         }
-        int maxDigit = nums.charAt(n-1) - '0';
-        int minDigit = nums.charAt(n-2) - '0';
-        int maxPos = n - 1;
-        int minPos = n - 2;
-        int maxNumber = num;
-        for (int i = n - 2; i >= 0; i--) {
-            int digitOne = (int) nums.charAt(i + 1) - '0';
-            int digitTwo = (int) nums.charAt(i) - '0';
-            if (maxDigit < digitOne) {
-                    maxDigit = digitOne;
-                    maxPos = i + 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int d = 9; d > digits[i] - '0'; d--) {
+                if (last[d] > i) {
+                    char c = digits[i];
+                    digits[i] = (char)('0' + d);
+                    digits[last[d]] = c;
+                    System.out.println(digits);
+                    return Integer.parseInt(new String(digits));
+                }
             }
+        }
 
-            if (minDigit >= digitTwo || digitTwo < maxDigit) {
-                    minDigit = digitTwo;
-                    minPos = i;
-            }
-            if (maxDigit > minDigit && maxPos > minPos) {
-                char minCharNum = (char) ('0' + minDigit);
-                char maxCharNum = (char) ('0' + maxDigit);
-                char tempOne = nums.charAt(maxPos);
-                char tempTwo = nums.charAt(minPos);
-                nums.setCharAt(maxPos, minCharNum);
-                nums.setCharAt(minPos, maxCharNum);
-                maxNumber = Math.max(maxNumber, Integer.parseInt(nums.toString()));
-                nums.setCharAt(maxPos, tempOne);
-                nums.setCharAt(minPos, tempTwo);
-            }
-        }
-        
-        return maxNumber;
+
+        return num;
     }
 }
+
+// 3366
