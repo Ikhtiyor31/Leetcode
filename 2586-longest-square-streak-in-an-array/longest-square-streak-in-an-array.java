7e9e71261f34f07fc2d9d15146cb54aa2e0711d8
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-10-28 11:39:03 +0900

    Time: 30 ms (90.63%) | Memory: 61.3 MB (36.11%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
@@ -5,21 +5,29 @@ class Solution {
         return Math.floor(sqrt) == sqrt;
     }
     public int longestSquareStreak(int[] nums) {
-        
-        Map<Integer, Integer> longestStreak = new HashMap<>();
         Arrays.sort(nums);
+
+        int[] array = new int[nums[nums.length - 1] + 1];
+        int maxNum = nums[nums.length - 1] + 1;
         for (int num: nums) {
-            if (isPerfectSquareRoot(num) && longestStreak.containsKey((int) Math.sqrt(num))) {
-                longestStreak.put(num, longestStreak.get((int) Math.sqrt(num)) + 1);
-            } else {
-                longestStreak.put(num, 1);
-            }
+            array[num] = 1;
         }
+        
+        
         int longest = 0;
-        for (int streak: longestStreak.values()) {
-            longest = Math.max(longest, streak);
+        for (int i = 0; i < nums.length; i++) {
+            long current = nums[i];
+            int count = 0;
+            while (array[(int) current] == 1) {
+                count++;
+                if (1e5 < current * current || maxNum <= current * current) 
+                    break;
+                current = current * current;
+            } 
+            longest = (int) Math.max(count, longest);
         }
-        return longest != 1 ? longest : - 1;
+
+        return longest > 1 ? longest : - 1;
 
     }
 
