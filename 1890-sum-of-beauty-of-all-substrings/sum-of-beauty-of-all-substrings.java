caefb0dc133b313f666d3b1f205eda9c7482745e
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-11-05 17:19:48 +0900

    Time: 62 ms (59.05%) | Memory: 42.6 MB (64.78%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int beautySum(String s) {
+        int n = s.length();
+        int[] freq = new int[26];
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            freq[c]++;
+            int minFreq = Integer.MAX_VALUE;
+            int maxFreq = 0;
+            for (int j = 0; j < 26; j++) {
+                maxFreq = Math.max(maxFreq, freq[j]);
+                if (freq[j] != 0) {
+                    minFreq = Math.min(minFreq, freq[j]);
+                }
+            }
+
+            if (minFreq != Integer.MAX_VALUE && maxFreq >= 2) {
+                answer += maxFreq - minFreq;
+            }
+        }
+        int k = 0;
+        while (k < n) {
+            freq = new int[26];
+            for (int i = k + 1; i < n; i++) {
+                int minFreq = Integer.MAX_VALUE;
+                int maxFreq = 0;
+                int c = s.charAt(i) - 'a';
+                freq[c]++;
+
+                for (int j = 0; j < 26; j++) {
+                    maxFreq = Math.max(maxFreq, freq[j]);
+                    if (freq[j] != 0) {
+                        minFreq = Math.min(minFreq, freq[j]);
+                    }
+                }
+
+                if (minFreq != Integer.MAX_VALUE && maxFreq >= 2) {
+                    answer += maxFreq - minFreq;
+                }
+            }
+            k++;
+        }
+        
+        return answer;
+    }
+}
+/* 
+
+s = "aabcbaa"
+       22234 
+       11111 
+       11123 = 8
+      211 
+*/
