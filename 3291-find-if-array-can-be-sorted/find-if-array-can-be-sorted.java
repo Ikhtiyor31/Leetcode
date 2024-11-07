8649ac2cec3fa84942b7bd12bd6fb47bfaf60651
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-11-07 14:16:33 +0900

    Time: 1 ms (100.00%) | Memory: 44.2 MB (62.50%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -1,55 +1,40 @@
 class Solution {
-    public void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    }
-    public boolean isSameSetbits(int a, int b) {
-        int c1 = 0;
-        int c2 = 0;
-        for (int i = 0; i < 32; i++) {
-            c1 += (a >> i) & 1;
-            c2 += (b >> i) & 1;
-        }
-     
-        return c1 == c2;
-    }
     public boolean canSortArray(int[] nums) {
-        
         int n = nums.length;
+        int prevSetbits = Integer.bitCount(nums[0]);
+        int maxSegment = nums[0];
+        int minSegment = nums[0];
+        int maxPrevSegment = Integer.MIN_VALUE;
+        
         for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[i-1]) {
-               break;
-            }
-            if (i == n - 1) {
-                return true;
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (nums[j] > nums[j+1] && isSameSetbits(nums[j], nums[j + 1])) {
-                    //System.out.println(nums[j] + " " + nums[j+1]);
-                    swap(nums, j, j+1);
+            int currSetbits = Integer.bitCount(nums[i]);
+            if (currSetbits == prevSetbits) {
+                maxSegment = Math.max(maxSegment, nums[i]);
+                minSegment = Math.min(minSegment, nums[i]);
+            } else {
+                if (minSegment < maxPrevSegment) {
+                    return false;
                 }
-            }
-        }
-        System.out.println(Arrays.toString(Arrays.stream(nums).toArray()));
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[i-1]) {
-                return false;
-            }
-        }
 
+                maxPrevSegment = maxSegment;
+                maxSegment = nums[i];
+                minSegment = nums[i];
+                prevSetbits = currSetbits;
+            }
+        }
+        if (minSegment < maxPrevSegment) {
+            return false;
+        }
         return true;
     }
 }
 
-/*
-[8, 4, 2, 30, 15]
+/* make the separate groups on contigouos way 
 
-
-
-
+ [8,4,2,16,30,15]
+ 8, 4, 2   30,15
+    1        4
+keep track of prev max segment to compare subsequent segment min element
+if (sebsequent_min_element < maxPrevSegment) return false;
 
 */
