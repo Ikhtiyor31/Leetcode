5f4a413e36514c8df4c177d3d23f3095f323f338
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-11-08 11:26:44 +0900

    Time: 3 ms (78.95%) | Memory: 59.3 MB (30.04%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.java
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] getMaximumXor(int[] nums, int maximumBit) {
         int n = nums.length;
-        int[] prefixXOR = new int[n];
-        
-        prefixXOR[0] = nums[0];
+        int result = nums[0];
         for (int i = 1; i < n; i++) {
-            prefixXOR[i] = prefixXOR[i-1] ^ nums[i];
+            result ^= nums[i];
         }
-
+        int[] ret = new int[n];
         for (int i = 0; i < n; i++) {
-            nums[i] = (prefixXOR[n-i-1] ^ ((1 << maximumBit) - 1));
+            ret[i] = result ^ ((1 << maximumBit) - 1);
+            result ^= nums[n-i-1];
         }
 
-
-        return nums;
+        return ret;
         
     }
 }
