22286e5e4395427062359a93d30fa488f684417e
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-11-11 13:45:18 +0900

    Time: 7 ms (18.42%) | Memory: 45 MB (22.52%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.java
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -1,62 +1,40 @@
 class Solution {
-    private int findMaxPrimeLessThan(int current, int next) {
-        // Iterate from current - 1 down to 2 to find the largest prime
-        for (int i = 1; i < current; i++) {
-            if (isPrime(i) && current - i < next) {
-                return current - i;
-            }
-        }
-        return -1;
-    }
-
-    private boolean isPrime(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
+    private boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i  == 0)
                 return false;
-            }
         }
+
         return true;
     }
-
     public boolean primeSubOperation(int[] nums) {
         int n = nums.length;
-        for (int i = 1; i < n; i++) {
-            if (nums[i-1] >= nums[i]) {
-                break;
+        int maxElement = Arrays.stream(nums).max().getAsInt();
+        int[] previousPrimes = new int[maxElement + 1];
+        for (int i = 2; i <= maxElement; i++) {
+            if (isPrime(i)) {
+                previousPrimes[i] = i;
+            } else {
+                previousPrimes[i] = previousPrimes[i-1];
             }
-            if (i == n - 1) return true;
-        }
-       
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                continue;
-            }
-            int result = findMaxPrimeLessThan(nums[i], nums[i+1]);
-            if (result == -1) {
-                return false;
-            }
-            nums[i] = result != -1 ? result : nums[i];
-            //System.out.println(nums[i]);
         }
 
-        for (int i = 1; i < n; i++) {
-           
-            if (nums[i-1] >= nums[i]) {
+        for (int i = 0; i < n; i++) {
+            int bound = 0;
+            if (i == 0) {
+                bound = nums[i];
+            } else {
+                bound = nums[i] - nums[i-1];
+            }
+
+            if (bound <= 0) {
                 return false;
             }
+
+            nums[i] -= previousPrimes[bound - 1];
+
         }
-       
 
         return true;
     }
 }
-
-/*
-
-[4, 9, 6, 10]
-
- 0  1  2   3
-           
-
-*/
