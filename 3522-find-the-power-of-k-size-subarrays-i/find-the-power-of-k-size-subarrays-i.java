98456e4d7b0f57907ceb8efa8493e23f735e8f1e
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-11-19 13:04:16 +0900

    Time: 1 ms (100.00%) | Memory: 45.6 MB (33.19%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
@@ -1,42 +1,28 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
         int n = nums.length;
-        int[] ans = new int[n - k + 1];
-        int counter = 0;
-        for (int i = 0; i < k ; i++) {
-            if (i == 0) {
-                counter = 1;
-            } else {
-                if (nums[i - 1] + 1 != nums[i]) {
-                    counter = 1;
-                } else {
-                    counter += 1;
-                }
-            }
-        }
-        int j = 0;
-        if (counter == k) {
-            ans[j++] = nums[k-1];
-        } else {
-            ans[j++] = -1;
-        }
-
-        for (int i = k; i < n; i++) {
-            if (nums[i - 1] + 1 == nums[i]) {
-                counter++;
-            } else {
-                counter = 1;
-            }
-            if (counter > k) {
-                counter--;
-            }
-            if (counter == k) {
-                ans[j++] = nums[i];
-            } else {
-                ans[j++] = -1;
-            }
+        if (k == 1) {
+            return nums;
         }
         
+        int[] ans = new int[n - k + 1];
+        int consecutiveCount = 1;
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i-1] + 1 == nums[i]) {
+                consecutiveCount++;
+            } else {
+                consecutiveCount = 1;
+            }
+            
+            if (consecutiveCount >= k || k == 1) {
+                
+                ans[i - k + 1] = nums[i];
+            } else if (i + 1 >= k) {
+                ans[i - k + 1] = -1;
+            }
+        }
+
         return ans;
     }
 }
