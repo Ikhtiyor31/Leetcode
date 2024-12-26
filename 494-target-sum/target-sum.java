26343b419ca4a8c9dcf1fe61f226d4dc78689504
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-12-26 14:54:24 +0900

    Time: 622 ms (26.29%) | Memory: 41.1 MB (86.70%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,15 +1,20 @@
 class Solution {
     public int backtrack(int[] nums, int target, int current, int index) {
-        
-        if (index == nums.length) {
+         
+        if (index >= nums.length) {
             if (current == target) {
                 return 1;
             }
             return 0;
         }
        
-        return backtrack(nums, target, +nums[index] + current, index + 1) +  backtrack(nums, target, (-nums[index]) + current, index + 1);
+        int count = 0;
+
+        count += backtrack(nums, target, nums[index] + current, index + 1);
+        count += backtrack(nums, target, (nums[index] * -1) + current, index + 1);
         
+
+        return count;
     }
     public int findTargetSumWays(int[] nums, int target) {
         return backtrack(nums, target, 0, 0);
