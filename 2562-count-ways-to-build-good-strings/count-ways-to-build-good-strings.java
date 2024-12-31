b1c545eee33ed21331c2570b003f5dae8bebf61b
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2024-12-31 12:20:41 +0900

    Time: 437 ms (5.37%) | Memory: 77.2 MB (5.37%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
@@ -1,30 +1,43 @@
 class Solution {
     int mod = 1_000_000_007;
-    public int dfs(int i, int zero, int one, int low, int high, Map<Integer, Integer> memo) {
-        if (i > high) {
+    public int pleaseTry(int low, int high, String zero, String one, int form, Map<Integer, Integer> memo) {
+        if (form > high) {
             return 0;
-        } 
-        if (memo.containsKey(i)) {
-            return memo.get(i);
-        }   
+        }
+        if (memo.containsKey(form)) {
+            return memo.get(form);
+        }
+        int count = form  >= low ? 1 : 0;
+        count += pleaseTry(low, high, zero, one, form + zero.length(), memo);
+        count %= mod;
+        count += pleaseTry(low, high, zero, one, form + one.length(), memo);
+        count %= mod;
 
-        int count = i >= low ? 1 : 0;
-        count += dfs(i + zero, zero, one, low, high, memo);
-        count %= mod;
-        count += dfs(i + one, zero, one, low, high, memo);
-        count %= mod;
-        memo.put(i, count);
+        memo.put(form, count);
         return count;
+        
     }
-
     public int countGoodStrings(int low, int high, int zero, int one) {
         
+        String zeros = "";
+        String ones = "";
+        for (int i = 1; i <= zero; i++) {
+            zeros += "0";
+        }
 
-        int answer = 0;
+        for (int i = 1; i <= one; i++) {
+            ones += "1";
+        }
+
         Map<Integer, Integer> memo = new HashMap<>();
-        answer += dfs(0, zero, one, low, high, memo);
-        answer %= mod;
-        
-        return answer;
+        return pleaseTry(low, high, zeros, ones, 0, memo);
+     
     }
 }
+/*
+ low = 2, high = 3, zero = 1, one = 2 // "0" "11"
+
+        
+
+
+*/
