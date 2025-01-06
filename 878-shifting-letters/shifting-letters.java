a5b3a9f699e46042849ba46f11d2aefc4afa7a35
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-06 16:50:56 +0900

    Time: 11 ms (32.82%) | Memory: 58.4 MB (43.96%) - LeetSync

--- 878-shifting-letters/shifting-letters.java
+++ 878-shifting-letters/shifting-letters.java
@@ -3,20 +3,16 @@ class Solution {
 
         int sum = 0;
         int[] prefix = new int[shifts.length];
-        for (int i = shifts.length - 1; i >= 0; i--) {
-            sum += shifts[i];
-            sum %= 26;
-            prefix[i] = sum;
+        for (int shift: shifts) {
+            sum = (sum + shift) % 26;
            
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            sum = prefix[i];
             int ascii = (int)(s.charAt(i) - 'a');
             int shifted = (sum + ascii) % 26;
             sb.append((char)('a' + shifted));
-            
-            
+            sum = ((sum - shifts[i]) % 26 + 26) % 26;
         }
 
         return sb.toString();
