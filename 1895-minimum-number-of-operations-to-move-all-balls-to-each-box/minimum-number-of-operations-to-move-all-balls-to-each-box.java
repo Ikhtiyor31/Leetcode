103b6c255deeae45ee78acafb37e5d60e4a5844e
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-06 18:54:09 +0900

    Time: 4 ms (76.56%) | Memory: 45.3 MB (11.46%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,19 +1,39 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
         int[] answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (boxes.charAt(j) == '1')
-                    answer[i] += (i - j);
+        int ball = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1) {
+                suffix[i] += suffix[i+1] + ball;
             }
-
-            for (int j = i + 1; j < n; j++) {
-                if (boxes.charAt(j) == '1')
-                    answer[i] += (j - i);
-            }
+            ball += boxes.charAt(i) == '1' ? 1 : 0;
+            
         }  
+        ball = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                prefix[i] += prefix[i-1] + ball;
+            }
+            ball += boxes.charAt(i) == '1' ? 1 : 0;
+        }
 
+        for (int i = 0; i < n; i++) {
+            answer[i] = prefix[i] + suffix[i];
+        }
+        
         return answer; 
     }
 }
+/*
+    0 1 2 3 4 5
+    0 0 1 0 1 1
+    11 8 5 3 1 0
+    0  0 0 1 2 4
+
+ balls = 1
+ operations = 11, 8,    
+
+*/
