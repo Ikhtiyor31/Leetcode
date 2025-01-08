c2da86dfae1d7c27e30723c82615f7f6f1e741f6
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-09 00:23:58 +0900

    Time: 37 ms (55.62%) | Memory: 67.3 MB (42.13%) - LeetSync

--- 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.java
+++ 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.java
@@ -1,41 +1,32 @@
 class Solution {
     public List<List<String>> displayTable(List<List<String>> orders) {
         int n = orders.size();
-        Map<Integer, Map<String, Integer>> displayTable = new TreeMap<>();
-        Set<String> foodNames = new HashSet<>();
+        Map<Integer, Map<String, Integer>> tableData = new TreeMap<>();
+        Set<String> foodNames = new TreeSet<>();
         for (List<String> order: orders) {
-            Map<String, Integer> temp;
             int tableNumber = Integer.parseInt(order.get(1));
-            if (displayTable.containsKey(tableNumber)) {
-                temp = displayTable.get(tableNumber);
-            } else {
-                temp = new HashMap<>();
-            }
-           
-            temp.put(order.get(2), temp.getOrDefault(order.get(2), 0) + 1);
-            displayTable.put(tableNumber, temp);  
+            String foodItem = order.get(2);
+
+            tableData.computeIfAbsent(tableNumber, k -> new HashMap<>())
+            .put(foodItem, tableData.get(tableNumber).getOrDefault(foodItem, 0) + 1);
+
             foodNames.add(order.get(2));
 
         }
-        List<List<String>> answer = new ArrayList<>();
-        List<String> list = new ArrayList<String>(foodNames); 
-        Collections.sort(list);
-        list.add(0, "Table");
-        answer.add(list);
+        List<String> header = new ArrayList<String>(foodNames); 
+        header.add(0, "Table");
 
-        for (Map.Entry<Integer, Map<String, Integer>> mapOrders: displayTable.entrySet()) {
+        List<List<String>> answer = new ArrayList<>();
+        answer.add(header);
+
+        for (Map.Entry<Integer, Map<String, Integer>> entry : tableData.entrySet()) {
             List<String> temp = new ArrayList<>();
-            for (String foodItem: list) {
-                if (mapOrders.getValue().containsKey(foodItem)) {
-                    temp.add(mapOrders.getValue().get(foodItem) + "");
-                } else {
-                    if (foodItem.equals("Table")) {
-                        temp.add(mapOrders.getKey() + "");
-                    } else 
-                    temp.add("0");
-                }
+            List<String> row = new ArrayList<>();
+            row.add(String.valueOf(entry.getKey()));  
+            for (String foodItem : header.subList(1, header.size())) {
+                row.add(String.valueOf(entry.getValue().getOrDefault(foodItem, 0)));
             }
-            answer.add(temp);
+            answer.add(row);
         }
 
         return answer;
