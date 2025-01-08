4e2b7ac55e675b5f2bb69a2598191c29b2a69782
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-08 10:34:57 +0900

    Time: 26 ms (35.59%) | Memory: 46.4 MB (68.06%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,37 +1,21 @@
 class Solution {
-    
     public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
-        int[] prefixSum = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (i == 0)
-                prefixSum[i] = nums[i];
-            else 
-                prefixSum[i] = prefixSum[i-1] + nums[i];
-        }
-
+        int n = nums.length;    
         Map<Integer, Integer> map = new HashMap<>();
 
-        map.put(0, 1);
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (map.containsKey(prefixSum[i] - k)) {
-                count += map.get(prefixSum[i] - k);
-            }
-            map.put(prefixSum[i], map.getOrDefault(prefixSum[i], 0) + 1);
-        }
 
-        return count;
+
+        map.put(0, 1);
+        int sum = 0;
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                answer += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+       
+        return answer;
     }
 }
-
-/*
-nums = [1, 2, 3] k = 3
-1 3 6
-
-
-
-
-
-
-*/
