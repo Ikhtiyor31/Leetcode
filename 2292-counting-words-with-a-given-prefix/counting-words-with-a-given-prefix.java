c90a7ae12c112d1c768f6a9398e1ff5a0f6a4d94
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-09 10:21:12 +0900

    Time: 4 ms (10.57%) | Memory: 46 MB (5.10%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,10 +1,42 @@
 class Solution {
+    class TrieNode {
+        TrieNode[] children;
+        int count;
+        TrieNode() {
+            children = new TrieNode[26];
+            count = 0;
+        }
+    }
+    TrieNode root = new TrieNode();
+    public void addWord(String word) {
+        TrieNode current = root;
+        for (char c: word.toCharArray()) {
+            int index = (int) c - 'a';
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+            current = current.children[index];
+            current.count++;
+        }
+    }
+
     public int prefixCount(String[] words, String pref) {
         
-        int count = 0; 
+        
         for (String word: words) {
-            if (word.startsWith(pref))
-                count++;
+            addWord(word);
+        }
+
+        TrieNode current = root;
+        int count = 200; 
+        for (char c: pref.toCharArray()) {
+            int index = c - 'a';
+            if (current.children[index] == null) {
+                count = 0;
+                break;   
+            }
+            current = current.children[index];
+            count = Math.min(count, current.count);
         }
 
         return count;
