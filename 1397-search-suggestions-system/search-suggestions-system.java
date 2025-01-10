2399637e3b292dda2e14fef4ac2a263e75ad1e6f
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-10 19:43:12 +0900

    Time: 22 ms (57.25%) | Memory: 50.3 MB (20.05%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -0,0 +1,52 @@
+class Solution {
+    class TrieNode {
+        TrieNode[] children;
+        List<String> suggestedProducts;
+        public TrieNode() {
+            children = new TrieNode[26];
+            suggestedProducts = new ArrayList<>();
+        }
+    }
+    TrieNode root = new TrieNode();
+    public void addWord(String word) {
+        TrieNode current = root;
+        for (char c: word.toCharArray()) {
+            int index = c - 'a';
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+            current = current.children[index];
+            current.suggestedProducts.add(word);
+            Collections.sort(current.suggestedProducts);
+            if (current.suggestedProducts.size() > 3) {
+                current.suggestedProducts.remove(current.suggestedProducts.size()-1);
+            }
+            
+        }
+    }
+
+
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        for (String product: products) {
+            addWord(product);
+        }
+        Arrays.sort(products);
+        List<List<String>> answer = new ArrayList<>();
+        TrieNode current = root;
+        for (char c: searchWord.toCharArray()) {
+            int index = c - 'a';
+            if (current.children[index] == null) {
+                while (answer.size() < searchWord.length()) {
+                    answer.add(new ArrayList<>());
+                }
+                break;
+            }
+
+            current = current.children[index];
+            answer.add(current.suggestedProducts); 
+            
+        }
+
+        return answer;
+    }
+}
