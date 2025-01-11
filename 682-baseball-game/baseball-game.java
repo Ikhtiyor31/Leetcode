0b16c863c02a864501158e2dfc56e5ebae953e9a
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-11 19:24:02 +0900

    Time: 2 ms (94.62%) | Memory: 41.4 MB (96.07%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -4,19 +4,17 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for (String operation: operations) {
-            if (!operation.equals("C") && !operation.equals("D") && !operation.equals("+")) {
-                stack.push(Integer.parseInt(operation));
+            if (operation.equals("C")) {
+                stack.pop();
+            } else if (operation.equals("D")) {
+                stack.push(stack.peek() * 2);
+            } else if (operation.equals("+")) {
+                int top = stack.pop();
+                int add = top + stack.peek();
+                stack.push(top);
+                stack.push(add);
             } else {
-                if (operation.equals("C")) {
-                    stack.pop();
-                } else if (operation.equals("D")) {
-                    stack.push(stack.peek() * 2);
-                } else {
-                    int top = stack.pop();
-                    int add = top + stack.peek();
-                    stack.push(top);
-                    stack.push(add);
-                }
+                stack.push(Integer.parseInt(operation));
             }
         }
 
