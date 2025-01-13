a634507a88b9dc59faa280a0cb3a28b87ccc03f9
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-13 16:42:13 +0900

    Time: 17 ms (81.09%) | Memory: 56.4 MB (23.11%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.java
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.java
@@ -1,8 +1,8 @@
 class Solution {
     public int minimizeMax(int[] nums, int p) {
-         Arrays.sort(nums); // Sorting helps in minimizing the difference for pairs
+         Arrays.sort(nums); 
         int left = 0;
-        int right = nums[nums.length - 1] - nums[0]; // Max possible difference
+        int right = nums[nums.length - 1] - nums[0]; 
         
         while (left < right) {
             int mid = (left + right) / 2;
@@ -21,7 +21,7 @@ class Solution {
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i + 1] - nums[i] <= maxDiff) {
                 pairs++;
-                i++; // Skip next as it's already paired
+                i++;
             }
             if (pairs >= p) return true;
         }
