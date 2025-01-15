a9174486f6c7efc420da99aedbd1c4df96fd1560
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-15 15:03:03 +0900

    Time: 8 ms (33.33%) | Memory: 42.4 MB (67.56%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.java
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -1,47 +1,26 @@
 class Solution {
     public String getHint(String secret, String guess) {
-        Set<Integer> visited1 = new HashSet<>();
-        Set<Integer> visited2 = new HashSet<>();
+        Map<Character, Integer> map = new HashMap<>();
         int bullCount = 0;
-        int cowCount = 0;
         for (int i = 0; i < secret.length(); i++) {
             if (secret.charAt(i) == guess.charAt(i)) {
                 bullCount++;
-                visited1.add(i);
+            } else {
+                map.put(secret.charAt(i), map.getOrDefault(secret.charAt(i), 0) + 1);
             }
         }
-        System.out.println(visited1);
-        for (int i = 0; i < secret.length(); i++) {
-            if (visited1.contains(i)) continue;
-             for (int j = 0; j < guess.length(); j++) {
-                if (secret.charAt(i) == guess.charAt(j) && !visited1.contains(j) && !visited2.contains(j)) {
+
+        int cowCount = 0;
+
+        for (int i = 0; i < guess.length(); i++) {
+            if (secret.charAt(i) != guess.charAt(i)) {
+                if (map.containsKey(guess.charAt(i)) && map.get(guess.charAt(i)) > 0) {
                     cowCount++;
-                    visited2.add(j);
-                    break;
+                    map.put(guess.charAt(i), map.get(guess.charAt(i)) - 1);
                 }
             }
         }
-       
-
-
-
 
         return bullCount + "A" + cowCount + "B";
     }
 }
-
-/*
-1807
-7810
-
-1 -> 7
-0 -> 1
-7 -> 0
-
-
-7 -> 1
-1 -> 0
-0 -> 7
-
-
-*/
