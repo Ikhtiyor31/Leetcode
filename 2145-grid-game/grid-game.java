726bf6dad9ccff363cefd3a2f5eecce1f3df832f
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-21 14:27:18 +0900

    Time: 3 ms (100.00%) | Memory: 57.1 MB (92.75%) - LeetSync

--- 2145-grid-game/grid-game.java
+++ 2145-grid-game/grid-game.java
@@ -1,17 +1,31 @@
 class Solution {
     public long gridGame(int[][] grid) {
-        long topSum = 0;
-        for (int num: grid[0]) {
-            topSum += num;
-        }
-        long bottomSum = 0;
+        
+        int n = grid[0].length;
         long minimumSum = Long.MAX_VALUE;
-
-        for (int i = 0; i < grid[0].length; i++) {
-            topSum -= grid[0][i];
-            minimumSum = Math.min(minimumSum, Math.max(topSum, bottomSum));
-            bottomSum += grid[1][i];
-        }
+        /*for (int i = 0; i < n; i++) {
+            long bottomSum = 0;
+            for (int j = 0; j < i; j++) {
+                bottomSum += grid[1][j];
+            }
+            long topSum = 0;
+            for (int j = i + 1; j < n; j++) {
+                topSum += grid[0][j];
+                System.out.print(grid[0][i]);
+            }
+            //System.out.println(topSum + " " + bottomSum);
+            minimumSum = Math.min(minimumSum, Math.max(bottomSum, topSum));
+        } */
+        long bottomSum = 0;
+        long topSum = 0;
+        for (int j = 0; j < n; j++) {
+            topSum += grid[0][j];
+        } 
+        for (int j = 0; j < n; j++) {
+            topSum -= grid[0][j];
+            minimumSum = Math.min(minimumSum, Math.max(bottomSum, topSum));
+            bottomSum += grid[1][j];
+        } 
 
         return minimumSum;
     }
