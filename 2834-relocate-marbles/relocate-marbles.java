54a375659f2a5461564d5f301341e43e57f3eb1c
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-21 18:15:11 +0900

    Time: 336 ms (6.36%) | Memory: 88.7 MB (5.45%) - LeetSync

---
+++ 2834-relocate-marbles/relocate-marbles.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> relocateMarbles(int[] nums, int[] moveFrom, int[] moveTo) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, Boolean> marbles = new TreeMap<>();
+        for (int i = 0; i < moveFrom.length; i++) {
+            if (!map.containsKey(moveFrom[i])) {
+                map.put(moveFrom[i], new ArrayList<>());
+            }
+            map.get(moveFrom[i]).add(moveTo[i]);
+        }
+
+        for (int num: nums) {
+            marbles.put(num, false);
+        }
+
+        for (int mfrom: moveFrom) {
+            int mto = map.get(mfrom).get(0);
+            map.get(mfrom).remove(0);
+            marbles.put(mfrom, true);
+            marbles.put(mto, false);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        System.out.println(marbles);
+        for (Map.Entry<Integer, Boolean> entries: marbles.entrySet()) {
+            if (!entries.getValue()) {
+                list.add(entries.getKey());
+            }
+        }
+        
+
+        return list;
+    }
+}
