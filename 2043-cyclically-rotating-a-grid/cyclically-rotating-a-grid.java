4a54fa862782174db2d58b33cd64d25c3253ab2f
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-23 02:00:04 +0900

    Time: 7 ms (15.38%) | Memory: 45.3 MB (76.92%) - LeetSync

--- 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.java
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.java
@@ -1,57 +1,54 @@
 class Solution {
+
     public int[][] rotateGrid(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-
-        for (int i = 0; i < Math.min(m, n) / 2; i++) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < Math.min(n, m) / 2; i++) {
             List<Integer> temp = new ArrayList<>();
-
-            // Collect top row (left to right)
             for (int j = i; j < n - i; j++) {
+                // System.out.print(grid[i][j] + " ");
                 temp.add(grid[i][j]);
             }
 
-            // Collect right column (top to bottom)
             for (int r = i + 1; r < m - i - 1; r++) {
+                // System.out.print(grid[r][n-i-1] + " ");
                 temp.add(grid[r][n - i - 1]);
             }
 
-            // Collect bottom row (right to left)
-            for (int j = n - i - 1; j >= i; j--) {
-                temp.add(grid[m - i - 1][j]);
-            }
-
-            // Collect left column (bottom to top)
-            for (int r = m - i - 2; r > i; r--) {
-                temp.add(grid[r][i]);
-            }
-
-            // Rotate the collected elements by k positions
-            int rotateBy = k % temp.size(); // Avoid unnecessary full rotations
-            Collections.rotate(temp, -rotateBy);
-
-            // Place back the rotated elements
-            int index = 0;
-
-            // Place top row (left to right)
             for (int j = i; j < n - i; j++) {
-                grid[i][j] = temp.get(index++);
+                // System.out.print(grid[m-i-1][n-j-1] + " ");
+                temp.add(grid[m - i - 1][n - j - 1]);
             }
 
-            // Place right column (top to bottom)
             for (int r = i + 1; r < m - i - 1; r++) {
-                grid[r][n - i - 1] = temp.get(index++);
+                // System.out.print(grid[m-r-1][i] + " ");
+                temp.add(grid[m - r - 1][i]);
             }
 
-            // Place bottom row (right to left)
-            for (int j = n - i - 1; j >= i; j--) {
-                grid[m - i - 1][j] = temp.get(index++);
+            if (temp.size() > 0) {
+                //System.out.println(temp);
+       
+                Collections.rotate(temp, temp.size() - k);
+                //System.out.println(temp);
+                int c = 0;
+                for (int j = i; j < n - i; j++) {
+                    grid[i][j] = temp.get(c++);
+                }
+
+                for (int r = i + 1; r < m - i - 1; r++) {
+                    grid[r][n - i - 1] = temp.get(c++);
+                }
+
+                for (int j = i; j < n - i; j++) {
+                    grid[m - i - 1][n - j - 1] = temp.get(c++);
+                }
+
+                for (int r = i + 1; r < m - i - 1; r++) {
+                    grid[m - r - 1][i] = temp.get(c++);
+                }
             }
 
-            // Place left column (bottom to top)
-            for (int r = m - i - 2; r > i; r--) {
-                grid[r][i] = temp.get(index++);
-            }
         }
 
         return grid;
