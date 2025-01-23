253b7c09833c68da6b061e922a34fa212bafd864
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-23 14:25:50 +0900

    Time: 624 ms (87.94%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
 SELECT 
-    user_id, time_stamp as last_stamp
+    user_id, MAX(time_stamp) as last_stamp
 FROM
     Logins l1
 WHERE 
-    time_stamp = (SELECT MAX(time_stamp) FROM Logins l2 where l2.user_id = l1.user_id AND SUBSTR(time_stamp, 1, 4) = "2020");
+    YEAR(time_stamp) = 2020
+GROUP BY 1;
