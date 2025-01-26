b07a13fffe91efa794abd87bb726a1688c16f96f
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-26 15:08:55 +0900

    Time: 87 ms (7.13%) | Memory: 60.4 MB (58.81%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,34 +1,30 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
-
         int n = temperatures.length;
-        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        int answer[] = new int[n];
         for (int i = n - 1; i >= 0; i--) {
             if (stack.isEmpty()) {
                 stack.push(i);
             } else {
-                while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
                     stack.pop();
                 }
-
+                
                 if (!stack.isEmpty()) {
-                    result[i] = stack.peek() - i;
+                    answer[i] = stack.peek() - i;
                 }
                 stack.push(i);
             }
         }
 
 
-        return result;
+        return answer;
     }
 }
 
 /*
-[73, 74, 75, 71, 69, 72, 76, 73]
-              1  1  0  0
-
-[73, 74, 75, 75, 75, 75, 76, 76]
-[69, 69, 69, 69, 69, 72, 73, 73]
+the idea is to keep track of non-increasing temperature values in stack
+as soon as we meet greater value than top of stack, we pop value from stack.
 
 */
