b4ca8b481ca95adb6f1fa1db2b4bd4c5e9ead59c
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-27 01:36:26 +0900

    Time: 187 ms (5.10%) | Memory: 57.5 MB (50.78%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,61 +1,72 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-
         int n = heights.length;
         int largestArea = 0;
-        /* this is O(N^2) solution
-        
-        for (int i = 0; i < n; i++) {
+        /*for (int i = 0; i < n; i++) {
             int l = i;
             int r = i;
-            int current = heights[i];
-            largestArea = Math.max(largestArea, current);
-            
-            while (l >= 0 && heights[l] >= current) {
+            largestArea = Math.max(largestArea, heights[i]);
+            while (l >= 0 && heights[l] >= heights[i]) {
                 l--;
             }
 
-            while (r < n && heights[r] >= current) {
+            while (r < n && heights[r] >= heights[i]) {
                 r++;
             }
 
-            largestArea = Math.max(largestArea, current * (r - l - 1));
-        } */
+            largestArea = Math.max(largestArea, heights[i] * (r - l - 1));
+        }*/
 
-        Stack<Integer> stack = new Stack<>();
-        int[] left = new int[n];
-        int[] right = new int[n];
-        
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] leftMinIndex = new int[n];
         for (int i = 0; i < n; i++) {
             if (stack.isEmpty()) {
-              stack.push(i);  
-              left[i] = 0;
+                stack.push(i);
+                leftMinIndex[i] = 0;
             } else {
                 while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
                     stack.pop();
                 }
-                left[i] = stack.isEmpty() ? 0 : stack.peek() + 1;
+               
+                leftMinIndex[i] = stack.isEmpty() ? 0 : stack.peek() + 1;
                 stack.push(i);
+                
             }
         }
-        stack.clear();
+
+        stack = new Stack<Integer>();
+        int[] rightMinIndex = new int[n];
         for (int i = n - 1; i >= 0; i--) {
             if (stack.isEmpty()) {
-              stack.push(i);  
-              right[i] = n - 1;
+                stack.push(i);
+                rightMinIndex[i] = n - 1;
             } else {
                 while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
                     stack.pop();
                 }
-                right[i] = stack.isEmpty() ? n - 1 : stack.peek() - 1;
+                
+                rightMinIndex[i] = stack.isEmpty() ? n -1 : stack.peek() - 1;
                 stack.push(i);
             }
         }
-        
+
+        System.out.println(Arrays.toString(leftMinIndex));
+        System.out.println(Arrays.toString(rightMinIndex));
+
         for (int i = 0; i < n; i++) {
-            largestArea = Math.max(largestArea, heights[i] * (right[i] - left[i] + 1));
+            largestArea = Math.max(largestArea, heights[i] * (rightMinIndex[i] - leftMinIndex[i] + 1));
         }
 
         return largestArea;
     }
 }
+
+/*
+
+the concept is basically to find first lower or equal number to current from left and right.
+to do this, we need two array to keep track minimum or equal. number in the left and minimum or equal to number in the right.
+we keep index of non-decreasing numbers in stack 
+l = 0 0 2 3 
+r = 
+0 
+*/
