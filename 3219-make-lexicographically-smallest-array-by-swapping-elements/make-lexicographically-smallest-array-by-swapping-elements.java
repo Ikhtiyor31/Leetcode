37b427d57f1cb8ded0bd54f0ba45e9733cc2faf4
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-30 23:23:18 +0900

    Time: 1205 ms (5.00%) | Memory: 86.3 MB (6.14%) - LeetSync

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+        int[] sortedNums = Arrays.copyOf(nums, n);
+        Arrays.sort(sortedNums);
+        int group = 0;
+        HashMap<Integer, Integer> numGroups = new HashMap<>();
+        Map<Integer, LinkedList<Integer>> groupToList = new HashMap<>();
+        
+        numGroups.put(sortedNums[0], group);
+        groupToList.put(group, new LinkedList<>(Arrays.asList(sortedNums[0])));
+        for (int i = 1; i < n; i++) {
+            if (Math.abs(sortedNums[i-1]-sortedNums[i]) <= limit) {
+                numGroups.put(sortedNums[i], group);
+            } else {
+                group++;
+                numGroups.put(sortedNums[i], group);
+            }
+
+            if (!groupToList.containsKey(group)) {
+                groupToList.put(group, new LinkedList<>());
+            }
+
+            groupToList.get(group).add(sortedNums[i]);
+        }
+       
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int g = numGroups.get(num);
+            System.out.println(g);
+            nums[i] = groupToList.get(g).pop();
+        }
+
+        return nums;
+    }
+}
