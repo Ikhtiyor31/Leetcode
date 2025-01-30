cb0768a550c8b259ce3695b6b841fc2739cb9bce
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-30 23:27:08 +0900

    Time: 930 ms (5.00%) | Memory: 79.6 MB (15.43%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
@@ -5,10 +5,10 @@ class Solution {
         Arrays.sort(sortedNums);
         int group = 0;
         HashMap<Integer, Integer> numGroups = new HashMap<>();
-        Map<Integer, LinkedList<Integer>> groupToList = new HashMap<>();
+        Map<Integer, ArrayList<Integer>> groupToList = new HashMap<>();
         
         numGroups.put(sortedNums[0], group);
-        groupToList.put(group, new LinkedList<>(Arrays.asList(sortedNums[0])));
+        groupToList.put(group, new ArrayList<>(Arrays.asList(sortedNums[0])));
         for (int i = 1; i < n; i++) {
             if (Math.abs(sortedNums[i-1]-sortedNums[i]) <= limit) {
                 numGroups.put(sortedNums[i], group);
@@ -18,7 +18,7 @@ class Solution {
             }
 
             if (!groupToList.containsKey(group)) {
-                groupToList.put(group, new LinkedList<>());
+                groupToList.put(group, new ArrayList<>());
             }
 
             groupToList.get(group).add(sortedNums[i]);
@@ -27,7 +27,8 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int num = nums[i];
             int g = numGroups.get(num);
-            nums[i] = groupToList.get(g).pop();
+            nums[i] = groupToList.get(g).get(0);
+            groupToList.get(g).remove(0);
         }
 
         return nums;
