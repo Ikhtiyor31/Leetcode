13f939413ff6747aeb3a6553f246174eb258ba33
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-01-31 10:36:13 +0900

    Time: 2 ms (99.91%) | Memory: 50.1 MB (31.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,32 +1,28 @@
 class Solution {
-    public void dfs(char[][] grid, int i, int j, Set<String> visited) {
-        String pair = i+","+j;
-        if (i >= grid.length || i < 0 || j >= grid[i].length || j < 0 || visited.contains(pair)) {
+    public void dfs(char[][] grid, int i, int j) {
+  
+        if (i >= grid.length || i < 0 || j >= grid[i].length || j < 0 || grid[i][j] == '0') {
             return;
         }
 
-        visited.add(pair);
+        grid[i][j] = '0';
 
-        if (grid[i][j] == '0') {
-            return;
-        }
-
-        dfs(grid, i + 1, j, visited);
-        dfs(grid, i, j + 1, visited);
-        dfs(grid, i - 1, j, visited);
-        dfs(grid, i, j - 1, visited);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
     }
 
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int count = 0;
-        Set<String> visited = new HashSet<>();
+ 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited.contains(i+","+j) && grid[i][j] == '1') {
+                if (grid[i][j] == '1') {
                     count++;
-                    dfs(grid, i, j, visited);
+                    dfs(grid, i, j);
                 }
             }
         }
