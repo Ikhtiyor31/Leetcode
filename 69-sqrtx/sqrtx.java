7cf078ab4a68363c38657956ee97aed477e54413
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-02-04 21:01:11 +0900

    Time: 1 ms (86.92%) | Memory: 40.1 MB (99.85%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,21 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x <= 1) return x;
+        if (x <= 1) {
+            return x;
+        }
 
-        int r = x / 2;
         int l = 1;
-        while (l < r) {
-            int m = (l + (r - l) / 2) + 1;
-            if (m == x / m) {
-                return m;
-            } else if (m < x / m) {
-                l = m;
+        int r = x;
+        int ans = 0;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (m <= x / m) {
+                ans = m;
+                l = m + 1;
             } else {
                 r = m - 1;
             }
         }
 
+        return ans;
 
-        return l;
     }
 }
