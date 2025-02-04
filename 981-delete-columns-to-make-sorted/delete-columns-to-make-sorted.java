be8a6a563ea77a0650209af97169bcfbe534dee0
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-02-04 11:58:22 +0900

    Time: 8 ms (65.83%) | Memory: 44.3 MB (76.60%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -1,16 +1,11 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
         
-        List<char[]> grid = new ArrayList<>();
-        for (String str: strs) {
-            char[] arr = str.toCharArray();
-            grid.add(arr);
-        }
         int count = 0;
-        for (int i = 0; i < grid.get(0).length; i++) {
+        for (int i = 0; i < strs[0].length(); i++) {
             ///System.out.println(Arrays.toString(grid.get(i)));
-            for (int j = 1; j < grid.size(); j++) {
-                if (grid.get(j-1)[i] > grid.get(j)[i]) {
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j-1].charAt(i) > strs[j].charAt(i)) {
                     count++;
                     break;
                 }
