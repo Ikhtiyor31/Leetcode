c9cfc5cbbb30d1c6deb9a34bbb69f1890c5304a2
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-02-05 23:55:26 +0900

    Time: 309 ms (5.04%) | Memory: 63.3 MB (5.85%) - LeetSync

--- 164-maximum-gap/maximum-gap.java
+++ 164-maximum-gap/maximum-gap.java
@@ -1,16 +1,24 @@
 class Solution {
     public int maximumGap(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
-        if (n == 1) {
-            return 0;
-        }
-        int max = 0;
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] < nums[i])
-                max = Math.max(max, nums[i] - nums[i-1]);
+        Map<Integer, Integer> map = new TreeMap<>();
+        if (n < 2) return 0;
+        int maxGap = 0;
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        return max;
+        int prev = -1;
+
+        for (Map.Entry<Integer, Integer> entries: map.entrySet()) {
+            if (prev == -1) {
+                prev = entries.getKey();
+            } else {
+                maxGap = Math.max(maxGap, entries.getKey() - prev);
+                prev = entries.getKey();
+            }
+        }
+
+        return maxGap;
     }
 }
