3963ae33ca96bbb83ea70e4a489fba5ad51ecbd2
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-02-05 12:20:57 +0900

    Time: 0 ms (100.00%) | Memory: 41.8 MB (39.72%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -2,29 +2,21 @@ class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
  
         int[] notEquals = new int[2];
-        Arrays.fill(notEquals, -1);    
-
-        System.out.println(notEquals[0]);
-
+        int diffCount = 0;
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
-                if (notEquals[0] == -1)
+                diffCount++;
+                if (diffCount > 2) return false;
+                if (diffCount == 1)
                     notEquals[0] = i;
                 else 
                     notEquals[1] = i;
             }
         }
 
-        if (notEquals[0] == -1 ||  notEquals[1] == -1) {
-            return notEquals[0] == notEquals[1];
-        }
-        
-        StringBuilder sb1 = new StringBuilder(s1);
-        char take = sb1.charAt(notEquals[0]);
-        sb1.setCharAt(notEquals[0], sb1.charAt(notEquals[1]));
-        sb1.setCharAt(notEquals[1], take);
 
-        return sb1.toString().equals(s2);
+
+        return (s1.charAt(notEquals[0]) == s2.charAt(notEquals[1]) && s1.charAt(notEquals[1]) == s2.charAt(notEquals[0]));
         
     }
 }
