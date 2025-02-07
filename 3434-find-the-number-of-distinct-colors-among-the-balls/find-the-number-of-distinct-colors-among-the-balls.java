2be71d9e9b32b248e8369b2f2956481456da2673
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-02-07 10:56:24 +0900

    Time: 43 ms (45.85%) | Memory: 97 MB (68.12%) - LeetSync

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] queryResults(int limit, int[][] queries) {
+        
+        int answer[] = new int[queries.length];
+         Map<Integer, Integer> label = new HashMap<>();
+        Map<Integer, Integer> distinct = new HashMap<>();
+        int indx = 0;
+        for (int[] query: queries) {
+            if (!label.containsKey(query[0])) {
+                label.put(query[0], query[1]);
+                distinct.put(query[1], distinct.getOrDefault(query[1], 0) + 1);
+            } else {
+                distinct.put(label.get(query[0]), distinct.get(label.get(query[0])) - 1);
+                if (distinct.get(label.get(query[0])) == 0) {
+                    distinct.remove(label.get(query[0]));
+                }
+                 label.put(query[0], query[1]);
+                distinct.put(query[1], distinct.getOrDefault(query[1], 0) + 1);
+            }
+            answer[indx++] = distinct.size();
+            
+        }
+        return answer;
+    }
+}
+
+/*
+
+[0, 3, 5, 4, 0, 0]
+
+4, 5, 3, 4
+set = 5, 3, 4
+count = 2
+
+[1, 2, 2, 4, 5, 0]
+ 
+ 1, 2, 2
+
+*/
