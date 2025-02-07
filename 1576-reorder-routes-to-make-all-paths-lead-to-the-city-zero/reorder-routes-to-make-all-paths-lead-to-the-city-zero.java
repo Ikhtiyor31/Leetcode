8dd7a3acd6013337d93f26e16acedb7a21eef246
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-02-07 22:38:13 +0900

    Time: 88 ms (36.30%) | Memory: 80.5 MB (19.02%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,30 +1,31 @@
 class Solution {
-    public int minReorder(int n, int[][] connections) {
-        Map<Integer, List<List<Integer>>> adj = new HashMap<>();
-        for (int[] connection : connections) {
-            adj.computeIfAbsent(connection[0], k -> new ArrayList<List<Integer>>())
-                    .add(Arrays.asList(connection[1], 1));
-            adj.computeIfAbsent(connection[1], k -> new ArrayList<List<Integer>>())
-                    .add(Arrays.asList(connection[0], 0));
+    public int dfs(boolean[] visited, Map<Integer, List<List<Integer>>> adj, int node) {
+        if (visited[node]) {
+            return 0;
+        }
+        visited[node] = true;
+        int count = 0;
+        for (List<Integer> list: adj.get(node)) {
+            if (!visited[list.get(0)]) {
+                count += list.get(1);
+                count += dfs(visited, adj, list.get(0));
+            }
         }
 
-        boolean[] visited = new boolean[n];
-        visited[0] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        int count = 0;
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            if (!adj.containsKey(node)) {
-                continue;
-            }
+        return count;
+    }
+    public int minReorder(int n, int[][] connections) {
+        Map<Integer, List<List<Integer>>> adj = new HashMap<>();
 
-            for (List<Integer> list : adj.get(node)) {
-                if (!visited[list.get(0)]) {
-                    count += list.get(1);
-                    visited[list.get(0)] = true;
-                    q.offer(list.get(0));
-                }
+        for (int[] connection: connections) {
+            adj.computeIfAbsent(connection[0], k -> new ArrayList<List<Integer>>()).add(Arrays.asList(connection[1], 1));
+            adj.computeIfAbsent(connection[1], k -> new ArrayList<List<Integer>>()).add(Arrays.asList(connection[0], 0));
+        }
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                count += dfs(visited, adj, i);
             }
         }
 
