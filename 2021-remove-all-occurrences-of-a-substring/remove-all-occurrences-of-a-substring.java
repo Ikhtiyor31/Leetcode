ef839dc8478e43cf4c45bc764f7979b0eba487f3
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-02-11 14:06:12 +0900

    Time: 6 ms (45.15%) | Memory: 44.9 MB (15.02%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -3,18 +3,16 @@ class Solution {
         StringBuilder sb = new StringBuilder();
 
         for (char c: s.toCharArray()) {
+            sb.append(c);
             int startIndex = sb.length() - part.length();
             int endIndex = startIndex + part.length();
             if (sb.length() >= part.length() && sb.substring(startIndex, endIndex).equals(part)) {
                 sb.delete(startIndex, endIndex);
             }
-            sb.append(c);
+           
         }
 
-        int startIndex = sb.length() - part.length();
-        if (sb.length() >= part.length() && sb.substring(startIndex, startIndex + part.length()).equals(part)) {
-            sb.delete(startIndex, startIndex + part.length());
-        }
+      
 
 
         return sb.toString();
