639bf40d1f5848427a8d98136851ead860c685c5
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-02-14 20:58:26 +0900

    Time: 84 ms (16.08%) | Memory: 61.9 MB (5.90%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -1,33 +1,22 @@
 class Solution {
     public long minimumTime(int[] time, int totalTrips) {
-        
-        int n = time.length;
-        int sum = Arrays.stream(time).max().getAsInt();
         long l = 0;
         long r = 100_000_000_000_000L;
-        //System.out.println(r);
-        long ans = 0;
-        while (l <= r) {
+        long answer = 0;
+        while (l < r) {
             long m = l + (r - l) / 2;
-            long count = 0;
-            for (int i = 0; i < n; i++) {
-                count += (m / time[i]);
+            long total = 0;
+            for (int i = 0; i < time.length; i++) {
+                total += (m / time[i]);
             }
-            //System.out.println(count + " " + m);
-            if (count >= totalTrips) {
-                ans = m;
-                r = m - 1;
+           
+            if (total < totalTrips) {
+                l = m  + 1;
             } else {
-                l = m + 1;
+                r = m;
             }
         }
 
-        return ans;
+        return l;
     }
-} 
-/*
-
-3/1
-9, 2
-9  4
-*/
+}
