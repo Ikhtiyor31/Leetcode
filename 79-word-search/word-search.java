6036a3ad93e6cdbcb43055cf9ea9862aa2bce6df
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-03-03 17:24:32 +0900

    Time: 134 ms (79.88%) | Memory: 41.6 MB (76.98%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean backtrack(char[][] board, String word, int i, int j, int index) {
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0) {
+            return false;
+        }
+
+        if (index == word.length() - 1) {
+            return word.charAt(index) == board[i][j];
+        }
+
+        if (board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+     
+        char prev = board[i][j];
+        board[i][j] = '#';
+        boolean result = false;
+        result |= backtrack(board, word, i + 1, j, index + 1);
+        result |= backtrack(board, word, i, j + 1, index + 1);
+        result |= backtrack(board, word, i - 1, j, index + 1);
+        result |= backtrack(board, word, i, j - 1, index + 1);
+        board[i][j] = prev;
+
+        return result;
+    }
+    public boolean exist(char[][] board, String word) {
+        
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    //System.out.println(board[i][j]);
+                    if (backtrack(board, word, i, j, 0)) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
