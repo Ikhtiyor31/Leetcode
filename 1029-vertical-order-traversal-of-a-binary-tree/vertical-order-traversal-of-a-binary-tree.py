7d1780ac3367314f7a1ced22259ec5a502eb4e4d
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-03-09 21:52:24 +0900

    Time: 0 ms (100.00%) | Memory: 17.9 MB (88.03%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -6,23 +6,17 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        vertical = {}
-    
+        vertical = defaultdict(list)
         def dfs(root: TreeNode, width: int, depth: int) -> None:
             if not root:
                 return
-            if width not in vertical:
-                vertical[width] = [(depth, root.val)]
-            else:
-                vertical[width].append((depth, root.val))
+            vertical[width].append((depth, root.val))
             dfs(root.left, width - 1, depth + 1)
             dfs(root.right, width + 1, depth + 1)
         dfs(root, 0, 0)
-        i = 0
+       
         sorted_dict = {k: vertical[k] for k in sorted(vertical)}
         answer = []
-        for _, key in enumerate(sorted_dict):
-            sorted_data = sorted(vertical[key], key=lambda x: (x[0], x[1]))
-            second_values = [x[1] for x in sorted_data]
-            answer.append(second_values)
+        for key in sorted(vertical.keys()):
+            answer.append([val for depth, val in sorted(vertical[key])])
         return answer
