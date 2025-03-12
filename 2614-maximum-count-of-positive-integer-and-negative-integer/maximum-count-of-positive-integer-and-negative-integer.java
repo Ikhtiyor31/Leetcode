74349da47c1437e15af3abd6acd1a65a6e40ec3a
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-03-12 11:41:59 +0900

    Time: 0 ms (100.00%) | Memory: 44.7 MB (96.35%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,13 +1,46 @@
 class Solution {
-    public int maximumCount(int[] nums) {
-        
-        int posCount = 0;
-        int negCount = 0;
-        for (int num: nums) {
-            if (num > 0) posCount++;
-            if (num < 0) negCount++;
+    public int lowerBound(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0;
+        int r = n - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+         
         }
 
+        return l;
+    }
+
+    public int binarySearch2(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0;
+        int r = n - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+           if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l;
+    }
+
+    public int maximumCount(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0] == 0? 0 : 1;
+        }
+        int pos = lowerBound(nums, 1);
+        int posCount = pos == n - 1 ? 0 : n - pos;
+        int negCount = lowerBound(nums, 0);
+        negCount = negCount == n - 1 ? n : negCount;
         return Math.max(posCount, negCount);
     }
 }
