54d9c44ee3ac3b952b986e0d90e67e200dcb439f
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-03-18 22:19:41 +0900

    Time: 3 ms (98.64%) | Memory: 55.6 MB (77.38%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -7,11 +7,11 @@ class Solution {
         for (int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
 
             while ((usedBits & nums[windowEnd]) != 0) {
-                usedBits ^= nums[windowStart];
+                usedBits -= nums[windowStart];
                 windowStart++;
             }
 
-            usedBits ^= nums[windowEnd];
+            usedBits += nums[windowEnd];
             maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
         }
 
