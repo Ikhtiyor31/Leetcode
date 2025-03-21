9253017e85c90fe477f454f8608075ba57beeeea
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-03-22 00:11:19 +0900

    Time: 58 ms (59.82%) | Memory: 47.2 MB (28.92%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
@@ -1,38 +1,49 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        Set<String> availableSupply = new HashSet<>();
+        int n = recipes.length;
+        // store available supplies 
+        Set<String> availableSupplies = new HashSet<>();
+        // Map recipe to its index 
+        Map<String, Integer> indegree = new HashMap<>();
+        // Map ingredient to recipes that need it
+        Map<String, List<String>> dependencyGraph = new HashMap<>();
+        // Initialize available supplies
         for (String supply: supplies) {
-            availableSupply.add(supply);
+            availableSupplies.add(supply);
         }
 
-        Queue<Integer> recipeIndex = new LinkedList<>();
-        for (int i = 0; i < recipes.length; i++) {
-            recipeIndex.add(i);
-        }
 
-        int lastSize = -1;
-        List<String> answer = new ArrayList<>();
-        while (availableSupply.size() > lastSize) {
-            lastSize = availableSupply.size();
-            int size = recipeIndex.size();
-            while (size-- > 0) {
-                int index = recipeIndex.poll();
-                boolean canCreate = true;
-                for (String ingredient : ingredients.get(index)) {
-                    if (!availableSupply.contains(ingredient)) {
-                        canCreate = false;
-                        break;
-                    }
-                }
-                if (!canCreate) {
-                    recipeIndex.add(index);
-                } else {
-                    availableSupply.add(recipes[index]);
-                    answer.add(recipes[index]);
+        for (int i = 0; i < n; i++) {
+            indegree.put(recipes[i], 0);
+            for (String ingredient: ingredients.get(i)) {
+                if (!availableSupplies.contains(ingredient)) {
+                    dependencyGraph.putIfAbsent(ingredient, new ArrayList<String>());
+                    dependencyGraph.get(ingredient).add(recipes[i]);
+                    indegree.put(recipes[i], indegree.get(recipes[i]) + 1);
                 }
             }
+        }
 
-            
+        Queue<String> queue = new LinkedList<>();
+        for (Map.Entry<String, Integer> entries: indegree.entrySet()) {
+            System.out.println(entries);
+            if (entries.getValue() == 0) {
+                queue.add(entries.getKey());
+            }
+        }
+
+        List<String> answer = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+            answer.add(current);
+            if (!dependencyGraph.containsKey(current)) continue;
+
+            for (String children: dependencyGraph.get(current)) {
+                indegree.put(children, indegree.getOrDefault(children, 0) - 1);
+                if (indegree.get(children) == 0) {
+                    queue.add(children);
+                }
+            }
         }
 
         return answer;
