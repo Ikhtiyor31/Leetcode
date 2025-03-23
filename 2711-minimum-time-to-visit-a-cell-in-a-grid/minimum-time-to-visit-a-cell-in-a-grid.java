279f3992886cbad83da05ecc09b5cdb563a73020
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-03-24 01:09:52 +0900

    Time: 1444 ms (5.14%) | Memory: 62.6 MB (6.78%) - LeetSync

---
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minimumTime(int[][] grid) {
+        if (grid[0][1] > 1 && grid[1][0] > 1) {
+            return -1;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+        pq.offer(new int[]{0, 0, grid[0][0]});
+         int[][] directions = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0,-1}};
+        Set<String> visited = new HashSet<>();
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            //System.out.println(curr[2]);
+            if (curr[0] == m - 1 && curr[1] == n - 1) {
+                return curr[2];
+            }
+            System.out.println(Arrays.toString(curr));
+            for (int[] dir: directions) {
+                int r = dir[0] + curr[0];
+                int c = dir[1] + curr[1];
+                String key = r + "," + c;
+                if (r < m && r >= 0 && c < n && c >= 0 && !visited.contains(key)) {
+                    int waitTime = ((grid[r][c] - curr[2]) % 2 == 0) ? 1 : 0;
+                    pq.offer(new int[]{r, c, Math.max(grid[r][c] + waitTime, curr[2] + 1)});
+                    visited.add(key);
+                }
+            }
+        }
+
+        return -1;
+    }
+}
