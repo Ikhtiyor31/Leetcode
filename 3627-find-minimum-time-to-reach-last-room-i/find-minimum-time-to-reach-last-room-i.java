022dc2d4722ad9eb2d6f27a192217c985eae66b4
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-03-24 00:00:09 +0900

    Time: 111 ms (5.05%) | Memory: 45.5 MB (15.66%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.java
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.java
@@ -1,55 +1,31 @@
 class Solution {
-   
-    public int minTimeToReach(int[][] moveTime) {
-        int n = moveTime.length;
-        int m = moveTime[0].length;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+    public int minTimeToReach(int[][] moveTime) {
+        int m = moveTime.length;
+        int n = moveTime[0].length;
+        int answer = Integer.MAX_VALUE;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
         pq.offer(new int[]{0, 0, 0});
-        int directions[][] = new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
+        int[][] directions = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0,-1}};
         Set<String> visited = new HashSet<>();
         while (!pq.isEmpty()) {
-
             int[] curr = pq.poll();
-            if (curr[1] == n - 1 && curr[2] == m - 1) {
-                return curr[0];
+            System.out.println(Arrays.toString(curr));
+            if (curr[0] == m - 1 && curr[1] == n - 1) {
+                return curr[2];
             }
-            System.out.println(curr[0] + " " + curr[1] + " " + curr[2]);
-            for (int [] dir: directions) {
-                int r = dir[0];
-                int c = dir[1];
-                int newRow = r + curr[1];
-                int newCol = c + curr[2];
-                String key = new StringBuilder(newRow + "," + newCol).toString();
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !visited.contains(key)) {
-                    int maxTime = Math.max(curr[0], moveTime[newRow][newCol]) + 1;
-                    pq.offer(new int[]{maxTime, newRow, newCol});
+            for (int[] dir: directions) {
+                int newr = curr[0] + dir[0];
+                int newc = curr[1] + dir[1];
+                String key = newr + "," + newc; 
+                if (newr < m && newr >= 0 && newc < n && newc >= 0 && !visited.contains(key)) {
+                    pq.add(new int[]{newr, newc, Math.max(curr[2], moveTime[newr][newc]) + 1});
                     visited.add(key);
                 }
             }
         }
 
-        return -1;
+        return answer;
+
     }
 }
-
-/* [
-    [0,0,0],
-    [0,0,0]
-    ] 
-    [
-     [4, 4],
-     [4, 4]
-     ]
-    
-    [
-      [0, 1],
-      [1, 2]
-    ]
-    [
-        [100, 8],
-        [100, 9]
-    ]
-    
-*/
-
