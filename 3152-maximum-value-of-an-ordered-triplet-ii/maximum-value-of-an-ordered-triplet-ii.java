fec66d582344e497497e56b6daa0a43195eef21f
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-03-28 09:27:23 +0900

    Time: 2 ms (96.62%) | Memory: 56.9 MB (91.89%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.java
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.java
@@ -2,17 +2,15 @@ class Solution {
     public long maximumTripletValue(int[] nums) {
         int n = nums.length;
         long answer = 0;
-        int[] prefix = new int[n];
-        prefix[0] = nums[0];
-        int[] suffix = new int[n];
-        suffix[n-1] = nums[n-1];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = Math.max(prefix[i - 1], nums[i]);
-            suffix[n-i-1] = Math.max(suffix[n-i], nums[n-i-1]);
+        int maxDiff = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            long result = (long)maxDiff * nums[i];
+            answer = Math.max(answer, result);
+            maxDiff = Math.max(maxDiff, max - nums[i]);
+            max = Math.max(max, nums[i]);
         }
-        for (int j = 1; j < n - 1; j++) {
-            answer = Math.max(answer, (long)(prefix[j-1] - nums[j]) * suffix[j+1]);
-        }
+
         return answer;
     }
 }
