4ee179928a631a8c6854320019b7d2a9c91769e8
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-03-31 16:26:50 +0900

    Time: 33 ms (84.33%) | Memory: 63.8 MB (20.15%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.java
@@ -1,6 +1,8 @@
 class Solution {
     public long putMarbles(int[] weights, int k) {
         int n = weights.length;
+        if (k == 1 || n == 1) 
+            return 0;
         int[] pairWeights = new int[n-1];
         for (int i = 0; i < n - 1; i++) {
             pairWeights[i] = weights[i] + weights[i+1];
@@ -8,10 +10,16 @@ class Solution {
 
         Arrays.sort(pairWeights, 0, n - 1);
         long answer = 0;
+        long sumLastK = 0;
+        long sumFirstK = 0;
         for (int i = 0; i < k - 1; i++) {
-            answer += pairWeights[n-2-i] - pairWeights[i];
+            sumLastK += pairWeights[n-2-i];
+            sumFirstK += pairWeights[i];
         }
 
-        return answer;
+        long maxScore = weights[0] + weights[n-1] + sumLastK;
+        long minScore = weights[0] + weights[n-1] + sumFirstK;
+
+        return maxScore - minScore;
     }
 }
