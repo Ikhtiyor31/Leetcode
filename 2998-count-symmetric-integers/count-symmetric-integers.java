c1d43efa0209d1ccc7696919b967dff0ad1c7369
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-04-11 11:08:11 +0900

    Time: 307 ms (5.05%) | Memory: 45.4 MB (5.05%) - LeetSync

---
+++ 2998-count-symmetric-integers/count-symmetric-integers.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int answer = 0;
+        for (int x = low; x <= high; x++) {
+            String str = String.valueOf(x);
+            if (str.length() % 2 == 1) continue;
+            String first = str.substring(0, str.length() / 2);
+            String last = str.substring(str.length() / 2);
+            int firstSum = first.chars()
+                .map(Character::getNumericValue)
+                .sum();
+            int lastSum = last.chars()
+                .map(Character::getNumericValue)
+                .sum();
+            if (firstSum == lastSum) {
+                answer += 1;
+            }
+        }
+
+        return answer;
+        
+    }
+}
