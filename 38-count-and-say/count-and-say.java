61278d119b5f3400a6110c134aba5c715c65fc4a
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-04-18 12:23:19 +0900

    Time: 8 ms (38.96%) | Memory: 41.5 MB (60.37%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,19 +1,18 @@
 class Solution {
-    public String generate(String str) {
-        int left = 0;
-        StringBuilder answer = new StringBuilder();
-        for (int right = 1; right <= str.length(); right++) {
-            if (right == str.length() || str.charAt(left) != str.charAt(right)) {
-                answer.append(right - left).append(str.charAt(left));
-                left = right;
-            }
-        }
-        return answer.toString();
-    }
+
     public String countAndSay(int n) {
         String prev = "1";
         for (int i = 1; i < n; i++) {
-            prev = generate(prev);
+            String str = prev; 
+            int left = 0;
+            StringBuilder answer = new StringBuilder();
+            for (int right = 1; right <= str.length(); right++) {
+                if (right == str.length() || str.charAt(left) != str.charAt(right)) {
+                    answer.append(right - left).append(str.charAt(left));
+                    left = right;
+                }
+            }
+            prev = answer.toString();
         
         }
 
