a4e5693b8857e1e729ceccd7628d41e3fe68e18c
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-07-20 23:50:30 +0900

    Time: 1643 ms (31.92%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,23 +1,33 @@
-SELECT 
-    'Low Salary' category,
-    COUNT(account_id) accounts_count
-FROM 
-    Accounts
-WHERE
-    income < 20000
+# Write your MySQL query statement below
+WITH low_salary AS (
+    SELECT 
+        'Low Salary' AS category,
+        COUNT(*) AS accounts_count
+    FROM 
+        Accounts
+    WHERE income < 20000
+),
+
+average_salary AS (
+    SELECT 
+        'Average Salary' AS category,
+        COUNT(*) AS accounts_count
+    FROM 
+        Accounts
+    WHERE income BETWEEN 20000 AND 50000
+),
+
+high_salary AS (
+    SELECT 
+        'High Salary' AS category,
+        COUNT(*) AS accounts_count
+    FROM 
+        Accounts
+    WHERE income > 50000
+)
+
+SELECT * FROM high_salary
 UNION ALL
-SELECT 
-    'Average Salary' category,
-    COUNT(account_id) accounts_count
-FROM 
-    Accounts
-WHERE
-    income >= 20000 AND income <= 50000
+SELECT * FROM low_salary
 UNION ALL
-SELECT 
-    'High Salary' category, 
-    COUNT(account_id) accounts_count
-FROM
-    Accounts
-WHERE
-    income > 50000
+SELECT * FROM average_salary
