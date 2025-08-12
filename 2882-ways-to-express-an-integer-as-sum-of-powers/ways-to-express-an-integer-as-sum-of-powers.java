17e7e04d27be0e0e9b59459b8b4e65aa40481985
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2025-08-12 10:15:42 +0900

    Time: 235 ms (35.79%) | Memory: 45.1 MB (43.68%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.java
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,33 +1,22 @@
 class Solution {
-    private final int mod = 1000000007;
+    int mod = 1000000007;
+    public int dfs(int current, int n, int x, int[][] memo) {
+        if (n == 0) return 1;
+        if (current > n) return 0;
+        if (memo[current][n] != -1) return memo[current][n];
+        // not take 
+        int leave = dfs(current + 1, n, x, memo);
+        // take 
+        int take = dfs(current + 1, n - (int)(Math.pow(current, x)), x, memo);
 
-    public int numberOfWays(int start, int n, int x, int[][] memo) {
-        if (n == 0) {
-            return 1;
-        }
-
-        if (start > n) {
-            return 0;
-        }
-        if (memo[start][n] != -1) {
-            return memo[start][n];
-        }
-
-
-        // not take
-        int notTake = numberOfWays(start + 1, n, x, memo);
-        // take
-        int sum = (int) Math.pow(start, x);
-        int take = numberOfWays(start + 1, n - sum, x, memo);
-
-        return memo[start][n] = (take + notTake) % mod;
+        return memo[current][n] = (leave  + take) % mod;
     }
-
     public int numberOfWays(int n, int x) {
-        int[][] memo = new int[301][n + 1];
-        for (int[] row : memo) {
+        int[][] memo = new int[301][n+1];
+        for (int[] row: memo) {
             Arrays.fill(row, -1);
         }
-        return numberOfWays(1, n, x, memo) % mod;
+        return dfs(1, n, x, memo);
+
     }
 }
