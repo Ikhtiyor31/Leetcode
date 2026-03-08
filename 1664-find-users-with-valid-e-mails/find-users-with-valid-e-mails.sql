79f72e91b36c47a48def8ed97d17c4fad1fa1264
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2026-03-08 15:57:20 +0900

    Time: 1054 ms (7.68%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
 SELECT 
-    *
+    user_id, name, mail
 FROM 
     Users
 WHERE 
-    mail 
-REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$';
+REGEXP_LIKE(mail, '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$', 'c');
