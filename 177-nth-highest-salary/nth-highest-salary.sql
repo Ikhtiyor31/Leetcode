3f7a21ecc315da1168f533e13535fd65e040aac6
Ikhtiyor Abduazizov <55629713+Ikhtiyor31@users.noreply.github.com>
2026-03-11 21:37:49 +0900

    Time: 393 ms (93.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    select salary
+      from (select salary, dense_rank() over(order by salary desc) as salaryRank from Employee) ranked
+      where salaryRank = N
+      limit 1
+  );
+END
